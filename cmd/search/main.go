30807919afaa88e489e1bb0b39d54eea35363055
Jakub Jarosz <jakub@jarosz.dev>
2023-07-12 23:12:33 +0100

    Add search using goroutines

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -3,5 +3,9 @@ package main
 import "github.com/qba73/gocp"
 
 func main() {
+	// run linear search
+	// gocp.RunSearch()
+
+	// run independent search - separate goroutines
 	gocp.RunSearch()
 }
--- search.go
+++ search.go
@@ -32,9 +32,35 @@ func GoogleLinear(query string) []Result {
 	return results
 }
 
+// GoogleGoroutines takes a query and run search for web, images and video
+// independently. Each gorouting runs search and put results on a channel c.
+// In the loop we run 3 itertions to get results 3x from the channel.
+func GoogleGoroutines(query string) []Result {
+	c := make(chan Result) // on this channel we will send query results
+
+	// launch 3 independent searches - each in its own goroutine!
+	go func() {
+		c <- Web(query)
+	}()
+	go func() {
+		c <- Image(query)
+	}()
+	go func() {
+		c <- Video(query)
+	}()
+
+	var results []Result
+	for i := 0; i < 3; i++ {
+		result := <-c
+		results = append(results, result)
+	}
+	return results
+}
+
 func RunSearch() {
 	start := time.Now()
-	results := GoogleLinear("golang")
+	//results := GoogleLinear("golang")
+	results := GoogleGoroutines("golang")
 	elapsed := time.Since(start)
 	fmt.Println(results)
 	fmt.Println(elapsed)
