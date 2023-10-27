40a619ab8683f585293ba87e6b08861ef3638ea6
Jakub Jarosz <jakub@jarosz.dev>
2023-10-27 12:18:10 +0100

    Update echo server

---
+++ echo/echo.go
@@ -0,0 +1,45 @@
+package echo
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func Run() error {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	if err := input.Err(); err != nil {
+		log.Println(err)
+	}
+}
