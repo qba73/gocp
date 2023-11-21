3dad2e39c660e370c7da86879104f82239b563a2
Jakub Jarosz <jakub@jarosz.dev>
2023-11-21 08:53:36 +0000

    Add a netcat client

---
+++ cmd/netcat3/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/qba73/gocp/netcat3"
+
+func main() {
+	netcat3.Run()
+}
--- echo/echo.go
+++ echo/echo.go
@@ -28,9 +28,19 @@ func Run() error {
 func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
 	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", shout)
+	fmt.Fprintln(c, "\t", toTitle(shout))
 	time.Sleep(delay)
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", "--- --- ---")
+}
+
+func toTitle(s string) string {
+	if s == "" {
+		return s
+	}
+	first := strings.ToUpper(string(s[0]))
+	return first + strings.ToLower(s[1:])
 }
 
 func handleConn(c net.Conn) {
