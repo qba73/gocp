942dbea78a374ce2570c716c47f62a12d2483dc7
Jakub Jarosz <jakub@jarosz.dev>
2023-12-15 09:36:12 +0000

    WIP - fixing deadlock

--- prodline/prodline.go
+++ prodline/prodline.go
@@ -7,17 +7,26 @@ import (
 )
 
 type Bakery struct {
-	Verbose        bool
-	Cakes          int
-	BakeTime       time.Duration
-	BakeStdDev     time.Duration
-	BakeBuf        int
-	NumIcers       int
-	IceTime        time.Duration
-	IceStdDev      time.Duration
-	IceBuf         int
+	Verbose bool
+	Cakes   int
+
+	BakeTime   time.Duration
+	BakeStdDev time.Duration
+	BakeBuf    int
+
+	NumIcers  int
+	IceTime   time.Duration
+	IceStdDev time.Duration
+	IceBuf    int
+
 	InscribeTime   time.Duration
 	InscribeStdDev time.Duration
+	InscribeBuf    int
+
+	NumPackers    int
+	PackingTime   time.Duration
+	PackingStdDev time.Duration
+	PackerBuf     int
 }
 
 type cake int
@@ -55,15 +64,36 @@ func (b *Bakery) icer(iced chan<- cake, baked <-chan cake) {
 // inscriber represents an entity (machine or a person) that
 // takes cakes from the icer and decorate them (inscribe).
 // inscriber is a last stage of the cake production line.
-func (b *Bakery) inscriber(iced <-chan cake) {
+func (b *Bakery) inscriber(inscribed chan<- cake, iced <-chan cake) {
+	// for i := 0; i < b.Cakes; i++ {
+	// 	c := <-iced
+	// 	if b.Verbose {
+	// 		fmt.Println("icing", c)
+	// 	}
+	// 	work(b.InscribeTime, b.InscribeStdDev)
+	// 	if b.Verbose {
+	// 		fmt.Println("finished", c)
+	// 	}
+	// }
 	for i := 0; i < b.Cakes; i++ {
 		c := <-iced
 		if b.Verbose {
-			fmt.Println("icing", c)
+			fmt.Println("inscribing", c)
 		}
 		work(b.InscribeTime, b.InscribeStdDev)
+		inscribed <- c
+	}
+}
+
+func (b *Bakery) packer(inscribed <-chan cake) {
+	for i := 0; i < b.Cakes; i++ {
+		c := <-inscribed
 		if b.Verbose {
-			fmt.Println("finished", c)
+			fmt.Println("packaging", c)
+		}
+		work(b.PackingTime, b.PackingStdDev)
+		if b.Verbose {
+			fmt.Println("finished packaging", c)
 		}
 	}
 }
@@ -73,11 +103,22 @@ func (b *Bakery) Work(runs int) {
 	for run := 0; run < runs; run++ {
 		baked := make(chan cake, b.BakeBuf)
 		iced := make(chan cake, b.IceBuf)
+		inscribed := make(chan cake, b.InscribeBuf)
+
+		// start baking
 		go b.baker(baked)
+
+		// start icing cakes
 		for i := 0; i < b.NumIcers; i++ {
 			go b.icer(iced, baked)
 		}
-		b.inscriber(iced)
+
+		// start inscribing cakes
+		b.inscriber(iced, inscribed)
+
+		// start packaging cakes for storage
+		// Packaging is the last step in the production line.
+		b.packer(inscribed)
 	}
 }
 
@@ -88,17 +129,26 @@ func work(d, stddev time.Duration) {
 
 func RunBakery() {
 	b := Bakery{
-		Verbose:        true,
-		Cakes:          10,
-		BakeTime:       2 * time.Second,
-		BakeStdDev:     500 * time.Millisecond,
-		BakeBuf:        3,
-		NumIcers:       2,
-		IceTime:        3 * time.Second,
-		IceStdDev:      500 * time.Millisecond,
-		IceBuf:         3,
+		Verbose: true,
+
+		Cakes:      3,
+		BakeTime:   2 * time.Second,
+		BakeStdDev: 500 * time.Millisecond,
+		BakeBuf:    3,
+
+		NumIcers:  2,
+		IceTime:   3 * time.Second,
+		IceStdDev: 500 * time.Millisecond,
+		IceBuf:    3,
+
 		InscribeTime:   3 * time.Second,
 		InscribeStdDev: 1 * time.Second,
+		InscribeBuf:    2,
+
+		NumPackers:    1,
+		PackingTime:   200 * time.Millisecond,
+		PackingStdDev: 100 * time.Millisecond,
+		PackerBuf:     1,
 	}
 	b.Work(1)
 }
