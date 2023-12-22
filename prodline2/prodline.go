228ad9db6aadeb637506e79e3d5375ec8d7dd1e6
Jakub Jarosz <jakub@jarosz.dev>
2023-12-22 10:11:35 +0000

    Simulate the prod line with generic stages

--- prodline2/prodline.go
+++ prodline2/prodline.go
@@ -8,14 +8,20 @@ import (
 
 type item int
 
+type Work func(d, stddev time.Duration)
+
+// ProductionLine represents an imaginary production line
+// that process N number of items per run.
 type ProductionLine struct {
 	Logger  log.Logger
 	Verbose bool
 	Items   int
 }
 
-type Work func(d, stddev time.Duration)
-
+// StartStage is a first stage in the production line.
+// At this stage items (products) are initially prepared
+// for furthe processing. This about this stage as a stage
+// where raw items are produced and sent to the next stage.
 func (pl *ProductionLine) StartStage(name string, out chan<- item, fn Work, d, stddev time.Duration) {
 	for i := 0; i < pl.Items; i++ {
 		c := item(i)
@@ -27,6 +33,10 @@ func (pl *ProductionLine) StartStage(name string, out chan<- item, fn Work, d, s
 	}
 }
 
+// Stage represents an intermittent or the final stage in an imaginary
+// production line. It processes items that are sent by initial stage
+// on the incomming channel (in), processes the items and forwards
+// them to the next stage by placing them onto the outging channel (out).
 func (pl *ProductionLine) Stage(name string, in <-chan item, out chan<- item, fn Work, d, stddev time.Duration) {
 	for i := range in {
 		if pl.Verbose {
@@ -37,6 +47,8 @@ func (pl *ProductionLine) Stage(name string, in <-chan item, out chan<- item, fn
 	}
 }
 
+// Run simulates running all stages of the production line.
+// It defines an initial and further stages of the line.
 func (pl *ProductionLine) Run() {
 	baked := make(chan item, 1)
 	iced := make(chan item, 1)
