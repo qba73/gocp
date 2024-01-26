d874fb00a3cc52db9edcd7bed58ad454d9d501fc
Jakub Jarosz <jakub@jarosz.dev>
2024-01-26 12:11:25 +0000

    wait for result

--- leader/leader.go
+++ leader/leader.go
@@ -1,7 +1,28 @@
 package leader
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func Run() {
 	fmt.Println("Running leader election simulation...")
+
+	WaitForSignal()
+}
+
+func WaitForSignal() {
+	ch := make(chan bool)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- true
+		fmt.Println("start leading")
+	}()
+
+	p := <-ch
+	fmt.Println("election confirmed: recv'd signal :", p)
+	time.Sleep(time.Second)
+	fmt.Println("=========")
 }
